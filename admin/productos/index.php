706d80bbb9e916966d8d8ec6860cd08e7e8755d0
Adrian <adrianrojasanchez@gmail.com>
2020-11-14 12:50:36 -0600

    ListarProductos agregado

--- admin/productos/index.php
+++ admin/productos/index.php
@@ -8,76 +8,19 @@ require_once '../../shared/header.php';
         <hr>
         <a class="btn btn-success text-white mb-3" href="/admin/productos/add.php">Agregar Producto</a>
 
-        <table class="table">
+        <table class="table" id="tablaListadoProductos">
             <thead class="thead-dark">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Categoría</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Precio</th>
-                    <th scope="col">Stock</th>
-                    <th scope="col">Acción</th>
-                </tr>
+
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Id de Categoria</td>
-                    <td>Otto</td>
-                    <td>el precio</td>
-                    <td>el stock</td>
-                    <td>
-                        <div class="dropdown show">
-                            <a class="btn btn-primary dropdown-toggle" href="#" data-toggle="dropdown">Acci&oacute;n</a>
 
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#">Editar</a>
-                                <a class="dropdown-item" href="#">Eliminar</a>
-                            </div>
-                        </div>
-                    </td>
-
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Id de Categoria</td>
-                    <td>Thornton</td>
-                    <td>el precio</td>
-                    <td>el stock</td>
-                    <td>
-                        <div class="dropdown show">
-                            <a class="btn btn-primary dropdown-toggle" href="#" data-toggle="dropdown">Acci&oacute;n</a>
-
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#">Editar</a>
-                                <a class="dropdown-item" href="#">Eliminar</a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Id de Categoria</td>
-                    <td>the Bird</td>
-                    <td>el precio</td>
-                    <td>el stock</td>
-                    <td>
-                        <div class="dropdown show">
-                            <a class="btn btn-primary dropdown-toggle" href="#" data-toggle="dropdown">Acci&oacute;n</a>
-
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#">Editar</a>
-                                <a class="dropdown-item" href="#">Eliminar</a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
             </tbody>
         </table>
     </div>
 </div>
 
 <?php require_once '../../shared/footer.php';?>
+<script type="text/javascript" src="/assets/js/pages/admin/productos/listar.js?v<?php echo date('his') ?>"></script>
 
 </body>
 
