53b521cacc4de75e06411be4f05b46267ecb9712
Adrian <adrianrojasanchez@gmail.com>
2020-11-15 20:49:08 -0600

    agregando subcategorias

---
+++ admin/subcategorias/add.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../../shared/header.php';
+Utils::isAdmin();
+
+$categoria_id = 0;
+
+if(isset($_GET['idCategoria']) ){
+    $categoria_id = $_GET['idCategoria'];
+}else{
+    header("Location: /index.php");
+}
+?>
+
+<div class="row p-2">
+
+    <div class="container_sombreado container">
+        <h3 class="text-center colorMarino">Añadir subcategoría</h3>
+        <hr>
+
+        <form id="addSubCategoria">
+
+            <input type="hidden" name="categoria_id" id="idCategoria" value="<?= $categoria_id ?>">
+
+            <?php include '../templates/addEditCategoria.php'?>
+
+            <input type="submit" class="btn btn-primary" value="Editar" />
+        </form>
+    </div>
+</div>
+
+<?php require_once '../../shared/footer.php';?>
+<script type="text/javascript" src="/assets/js/pages/admin/subcategorias/add.js?v=<?php echo date('his') ?>">
+</script>
+
+</body>
+
+</html>
